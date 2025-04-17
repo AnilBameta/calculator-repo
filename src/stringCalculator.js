b20b6aaff5c7822a5fbf92173011ad7d670c99f7
anilbameta <anilbameta@gmail.com>
2025-04-17 16:54:45 +0530

    removing consoles

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -4,18 +4,14 @@ export function add(numbers) {
     let delimiter = /,|\\n/; // default delimiters
   
     if (numbers.startsWith('//')) {
-      console.log("customDelimiter",numbers)
       const parts = numbers.split("\\n");
-      console.log("parts",parts)
       const customDelimiter = parts[0].substring(2); // strip //
       delimiter = new RegExp(customDelimiter);
       numbers = parts.slice(1).join('\n'); // the rest after the delimiter declaration
     }
   
     const tokens = numbers.split(delimiter).map(n => n.trim()).filter(n => n !== '');
-    console.log(tokens, "tokens");
     const numList = tokens.map(Number);
-    console.log(numList,"numList");
   
     const negatives = numList.filter(n => n < 0);
     if (negatives.length > 0) {
